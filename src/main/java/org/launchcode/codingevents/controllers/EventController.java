d8c24ca9379896fde9ef7e16e95d49acd5c67f92
Alejandro Pedreros <pedrerosalejandro_21@hotmail.com>
2023-02-22 20:04:45 -0600

    Validating Models in EventController class.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,9 +6,11 @@ import org.launchcode.codingevents.models.Event;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.w3c.dom.stylesheets.LinkStyle;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,12 +29,20 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create event");
+            model.addAttribute("errorMsg", "Bad data!");
+            return "events/create";
+        }
+
         EventData.add(newEvent);
         return "redirect:";
     }
